7997318963670940ff5440b59bbea385c2d7d0e8
Vincent <vcootjans@gmail.com>
2023-11-13 15:40:37 +0100

    Added more small projects

---
+++ Bus routes/Bus_routes.py
@@ -0,0 +1,17 @@
+import csv
+from typing import Dict, List
+import statistics
+
+
+class BusRoutes(object):
+    def num_buses_to_destination(self, routes, source, target):
+        """
+        :type routes: List[List[int]]
+        :type source: int
+        :type target: int
+        :rtype: int
+        """
+
+
+
+
